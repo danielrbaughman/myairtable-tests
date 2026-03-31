ab4d5666a1f7cbd106cad38833d8762102d06ad1
Daniel Baughman <danielrbaughman@icloud.com>
2026-03-31 12:51:34 -0400

    enabling python checks

--- checks.sh
+++ checks.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 
-# uv sync
-# uv run ruff check
-# uv run ty check
-# uv run pytest
-# uv run ruff format
+# Python
+uv sync
+uv run ruff check
+uv run ty check
+uv run ruff format
 
+# TypeScript / JavaScript
 if ! command -v nvm &> /dev/null; then
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fi
-
 nvm use
-
 yarn install
 yarn lint
 yarn format
